5ac79061b35d95c5e4e58db497175c6ba9fc9c71
manikazad <manikazad@gmail.com>
2016-11-13 11:50:20 +0530

    Manual merge from old repo.
    why??

--- production_system/models.py
+++ production_system/models.py
@@ -30,8 +30,8 @@ class MasterTag(models.Model):
 class Item(models.Model):
     """Can either be service or product"""
 
-    service = models.ForeignKey(MasterService, null=True)
-    product = models.ForeignKey(MasterProduct, null=True)
+    service = models.ForeignKey(MasterService, blank=True, null=True)
+    product = models.ForeignKey(MasterProduct, blank=True, null=True)
     order = models.ForeignKey(Order)
     tag_id = models.ForeignKey(MasterTag)
 
@@ -96,7 +96,7 @@ class Task(models.Model):
     is_success = models.BooleanField(default=True)
 
     def __str__(self):
-        return "{}. {}".format(self.id, self.user.user.username)
+        return "{}. {} - {}".format(self.id, self.user.user.username, self.process.label)
 
 
 class Production(models.Model):
