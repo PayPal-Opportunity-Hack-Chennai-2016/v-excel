44c53ba82a07435d332762cdf70b1bd4ae36cf6d
KunalB <kunal.bhagawati@yahoo.com>
2016-11-13 13:43:39 +0530

    Put in filters

--- v_excel_inventory/settings.py
+++ v_excel_inventory/settings.py
@@ -149,3 +149,8 @@ STATICFILES_DIRS = [
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',)
+}
